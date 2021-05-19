18cc838ce380c5d507df0677df37042e610bf357
LXGMAX <24214894+LXGMAX@users.noreply.github.com>
2021-05-19 17:55:42 +0800

    Modular operation

--- drink.py
+++ drink.py
@@ -4,9 +4,7 @@ from tkinter import messagebox
 
 numberCN = ('十二','一','二','三','四','五','六','七','八','九','十','十一')
 
-timeNum = time.localtime().tm_hour
-if timeNum > 11:
-	timeNum -= 12
+timeNum = time.localtime().tm_hour % 12
 
 timeStr = str(numberCN[timeNum])
 messagebox.showinfo("喂！", "喂 ！" + timeStr + "点几嚟！饮下水先！")
