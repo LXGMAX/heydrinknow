9fad6fc3f5836252c4c09afd5159af6338dd8333
LXGMAX <24214894+LXGMAX@users.noreply.github.com>
2021-05-19 18:34:37 +0800

    更新变量

--- drink.py
+++ drink.py
@@ -2,9 +2,8 @@ import time
 from tkinter import messagebox
 
 
-numberCN = ('十二','一','二','三','四','五','六','七','八','九','十','十一')
+number_map = ('十二','一','二','三','四','五','六','七','八','九','十','十一')
 
-timeNum = time.localtime().tm_hour % 12
+time_num = time.localtime().tm_hour % 12
 
-timeStr = str(numberCN[timeNum])
-messagebox.showinfo("喂！", "喂 ！" + timeStr + "点几嚟！饮下水先！")
+messagebox.showinfo("喂！", "喂 ！" + number_map[time_num] + "点几嚟！饮下水先！")
